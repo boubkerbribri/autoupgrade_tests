98d2381f7c395720944656740b7505e578c0a949
Boubker Bribri <boubker.bribri@prestashop.com>
2021-02-12 10:58:43 +0100

    Fix global vars

--- globals.js
+++ globals.js
@@ -1,4 +1,3 @@
-global.PS_VERSION = '1.7.4';
 global.PS_VERSION_UPGRADE_FROM = global.PS_VERSION;
 global.PS_VERSION_UPGRADE_TO = process.env.PS_VERSION_UPGRADE_TO || '1.7.8';
 global.DOWNLOAD_PATH = process.env.DOWNLOAD_PATH;
